eb79a1ce3998d92daa4735ae1716b0516dc63709
niu tech <jerzyglowacki@gmail.com>
2017-08-07 13:01:00 +0200

    Code cleanup

--- content.js
+++ content.js
@@ -9,34 +9,10 @@
         location.replace(url);
     }
 
-    // function getGwlUrl() {
-    //     return "https://googleweblight.com/?lite_url=" + encodeURIComponent(location.href);
-    // }
-    //
-    // function getOriginalUrl(url) {
-    //     return decodeURIComponent(url.substring(url.indexOf('?lite_url=') + 10).split('&')[0]);
-    // }
-
     function getCacheUrl(url) {
         url = new URL(url);
         return 'https://' + url.hostname.replace(/-/g, '--').replace(/\./g, '-') + '.cdn.ampproject.org/c/' + (url.protocol === "https:" ? 's/' : '') + url.hostname + url.pathname + url.search;
     }
-
-    // function setViewport(el, width) {
-    //     el.style.margin = "0 auto";
-    //     el.style.maxWidth = width;
-    // }
-
-    // function onClickGWL(e) {
-    //     var el = e.target;
-    //     while (el && el !== document) {
-    //         if (el.matches("a[href*='/?lite_url=']")) {
-    //             el.href = getOriginalUrl(el.href);
-    //             break;
-    //         }
-    //         el = el.parentNode;
-    //     }
-    // }
 
     function init() {
         var isAmp = html.hasAttribute("amp") || html.hasAttribute("⚡") || html.hasAttribute('mip');
@@ -44,19 +20,7 @@
         var linkCanonical = document.querySelector("link[rel='canonical']");
         var isGoogleCache = location.hostname.indexOf("cdn.ampproject.org") > -1;
         var liteAtfContent = document.getElementById("lite-atf-content");
-        //var hasTranscodingFailed = liteAtfContent && liteAtfContent.innerText.indexOf("Transcoding test failed") === 0;
 
-        // if (isAmp) {
-        //     setViewport(html, "1000px");
-        //     // if (linkCanonical) {
-        //     //     document.querySelectorAll("a[href^='" + linkCanonical.href + "']").forEach(function (a) {
-        //     //         a.href += "#noredirect=1";
-        //     //     });
-        //     // }
-        // } else if (liteAtfContent) {
-        //     setViewport(liteAtfContent, "800px");
-        //     //document.addEventListener("click", onClickGWL);
-        // } else
         if (isGoogleCache) {
             var fwdlink = document.querySelector(".fwdlnk");
             if (fwdlink) { //TODO: Check for 404 status and redirect
--- options.js
+++ options.js
@@ -3,23 +3,18 @@
 (function () {
     chrome.runtime.getBackgroundPage(function (background) {
         var autoMode = document.getElementById("automode");
-        //var gwlMode = document.getElementById("gwlmode");
         var devMode = document.getElementById("devmode");
         var cacheMode = document.getElementById("cachemode");
-        //var proxyMode = document.getElementById("proxymode");
         var saveDataMode = document.getElementById("savedatamode");
         var adBlockMode = document.getElementById("adblockmode");
         var ifAdBlockMode = document.getElementById("ifadblockmode");
         var excepted = document.getElementById("excepted");
         var blocked = document.getElementById("blocked");
-        //var reset = document.getElementById("reset");
         var saved = document.getElementById("saved");
 
         autoMode.checked = background.autoMode;
-        //gwlMode.checked = background.gwlMode;
         devMode.checked = background.devMode;
         cacheMode.checked = background.cacheMode;
-        //proxyMode.checked = background.proxyMode;
         saveDataMode.checked = background.saveDataMode;
         adBlockMode.checked = background.adBlockMode;
         ifAdBlockMode.hidden = !background.adBlockMode;
@@ -31,11 +26,6 @@
             background.autoMode = this.checked;
             triggerSaved();
         });
-        // gwlMode.addEventListener("change", function () {
-        //     localStorage.setItem("gwlMode", this.checked);
-        //     background.gwlMode = this.checked;
-        //     triggerSaved();
-        // });
         devMode.addEventListener("change", function () {
             localStorage.setItem("devMode", this.checked);
             background.devMode = this.checked;
@@ -46,12 +36,6 @@
             background.cacheMode = this.checked;
             triggerSaved();
         });
-        // proxyMode.addEventListener("change", function () {
-        //     localStorage.setItem("proxyMode", this.checked);
-        //     background.proxyMode = this.checked;
-        //     background.triggerProxy();
-        //     triggerSaved();
-        // });
         saveDataMode.addEventListener("change", function () {
             localStorage.setItem("saveDataMode", this.checked);
             background.saveDataMode = this.checked;
@@ -77,37 +61,10 @@
             background.setAdBlock();
             triggerSaved();
         });
-        // reset.addEventListener("click", function () {
-        //     localStorage.removeItem('totalBytes');
-        //     background.sessionBytes = background.sessionOriginalBytes = 0;
-        //     location.reload();
-        // });
 
         var triggerSaved = function () {
             saved.hidden = false;
             setTimeout(function () { saved.hidden = true; }, 1000);
         };
-
-        // document.getElementById('session-mbytes').innerText = Math.round(background.sessionBytes / 1048576); //MB
-        // document.getElementById('session-mbytes-original').innerText =  Math.round(background.sessionOriginalBytes / 1048576); //MB
-        // document.getElementById('session-percent').innerText = Math.round(100 - 100 * background.sessionBytes / background.sessionOriginalBytes); //%
-        //
-        // var totalBytes = JSON.parse(localStorage.getItem('totalBytes')) || {};
-        // var totalBytesSent = Object.keys(totalBytes).reduce(function(i, j) { return i + totalBytes[j][0]; }, 0);
-        // var totalBytesOriginal = Object.keys(totalBytes).reduce(function(i, j) { return i + totalBytes[j][1]; }, 0);
-        //
-        // document.getElementById('total-mbytes').innerText = Math.round(totalBytesSent / 1048576); //MB
-        // document.getElementById('total-mbytes-original').innerText =  Math.round(totalBytesOriginal / 1048576); //MB
-        // document.getElementById('total-percent').innerText =  Math.round(100 - 100 * totalBytesSent / totalBytesOriginal); //%
-
-        // if (Object.keys(totalBytes).length) {
-        //     var cumulativeBytesSent = Object.keys(totalBytes).reduce(function(i, j) { i.push((i.length && i[i.length-1] || 0) + Math.round(totalBytes[j][0] / 1048576)); return i; }, []);
-        //     var cumulativeBytesOriginal = Object.keys(totalBytes).reduce(function(i, j) { i.push((i.length && i[i.length-1] || 0) + Math.round(totalBytes[j][1] / 1048576)); return i; }, []);
-        //     new Chartist.Line('.ct-chart', {labels: Object.keys(totalBytes), series: [cumulativeBytesSent, cumulativeBytesOriginal]}, {showArea: true});
-        // }
-
-        // setInterval(function() {
-        //     location.reload();
-        // }, 300000); //Refresh stats every 5 minutes
     });
 }());
